0b24afbdff3c9ba02b134d0133fee02984a687ac
Adrian Webb <adrian@webb.sh>
2025-05-27 17:44:13 -0400

    Updating the secret and config map generation to include the option of an environment specific configuration in the namespace module locals manifest.

--- modules/namespace/locals.tf
+++ modules/namespace/locals.tf
@@ -2,35 +2,49 @@
 # Namespace secrets
 #
 locals {
-  secrets_path = "${var.path}/secrets.yaml"
+  secrets_path     = "${var.path}/secrets.yaml"
+  env_secrets_path = "${var.path}/secrets.${var.variables.environment}.yaml"
 
-  secrets_raw = fileexists(local.secrets_path) ? yamldecode(
+  secrets_raw = fileexists(local.env_secrets_path) ? yamldecode(
+    file(local.env_secrets_path)
+    ) : fileexists(local.secrets_path) ? yamldecode(
     file(local.secrets_path)
   ) : null
-  core_secret_names = local.secrets_raw != null ? [
+
+  secret_names = local.secrets_raw != null ? [
     for name, config in local.secrets_raw : name
   ] : []
 
-  secret_names = toset(local.core_secret_names)
-  secrets      = fileexists(local.secrets_path) ? sensitive(yamldecode(templatefile(local.secrets_path, var.variables))) : null
+  secrets = fileexists(local.env_secrets_path) ? sensitive(yamldecode(
+    templatefile(local.env_secrets_path, var.variables)
+    )) : fileexists(local.secrets_path) ? sensitive(yamldecode(
+    templatefile(local.secrets_path, var.variables)
+  )) : null
 }
 
 #
 # Namespace config maps
 #
 locals {
-  config_path = "${var.path}/config.yaml"
+  config_path     = "${var.path}/config.yaml"
+  env_config_path = "${var.path}/config.${var.variables.environment}.yaml"
 
-  config_raw = fileexists(local.config_path) ? yamldecode(
+  config_raw = fileexists(local.env_config_path) ? yamldecode(
+    file(local.env_config_path)
+    ) : fileexists(local.config_path) ? yamldecode(
     file(local.config_path)
   ) : null
-  core_config_names = local.config_raw != null ? [
+
+  config_names = local.config_raw != null ? [
     for name, config in local.config_raw : name
   ] : []
 
-  config_names = toset(local.core_config_names)
+  config_full = fileexists(local.env_config_path) ? yamldecode(
+    templatefile(local.env_config_path, var.variables)
+    ) : fileexists(local.config_path) ? yamldecode(
+    templatefile(local.config_path, var.variables)
+  ) : null
 
-  config_full = fileexists(local.config_path) ? yamldecode(templatefile(local.config_path, var.variables)) : null
   config = local.config_full != null ? try(
     nonsensitive(local.config_full),
     local.config_full

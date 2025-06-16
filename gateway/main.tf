7c7aa61053600c1c3c81b91172fafd7c078139f0
Adrian Webb <adrian@webb.sh>
2025-06-16 04:39:03 -0400

    Updating the ArgoCD configuration path and the top level project path in the gateway module.

--- gateway/main.tf
+++ gateway/main.tf
@@ -5,8 +5,8 @@ module "kubernetes_apps" {
   environment = var.environment
 
   role_groups        = local.access_config.argocd
-  argocd_config_path = "${var.project_path}/argocd"
-  project_path       = "${var.project_path}/projects"
+  argocd_config_path = "${var.project_path}/deploy/argocd"
+  project_path       = "${var.project_path}/deploy"
 
   project_sequence = var.argocd_project_sequence
   variables        = local.variables
